4dd207f81521cb0981a6634b81d5104fc59c5bd8
palashjhabak <palash@techlift.in>
2019-07-31 12:03:34 +0530

    changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,12 @@ while ! mysqladmin ping -hmariadb --silent; do
     sleep 1
 done
 
+cd /home/frappe/frappe-bench/sites
+if [ ! -d "site1.local" ]; then
+	cd /home/frappe/frappe-bench
+	bench new-site site1.local --mariadb-root-password 123 --force
+	bench get-app --branch master erpnext 
+	bench --site site1.local install-app erpnext
+fi	
 cd /home/frappe/frappe-bench
-bench new-site site1.local --mariadb-root-password 123 --force
-bench get-app --branch master erpnext 
-bench --site site1.local install-app erpnext
 bench start
